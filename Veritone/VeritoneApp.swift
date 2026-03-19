09bdea509dac28558129e5e607c201184316f86a
synerr <davydovmd@proton.me>
2026-03-19 14:33:21 +0700

    Initial Commit

---
+++ Veritone/VeritoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeritoneApp.swift
+//  Veritone
+//
+//  Created by Михаил Давыдов on 19.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct VeritoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
